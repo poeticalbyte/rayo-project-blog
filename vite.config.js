90d9b921e69b9945c908809028118d9bc2b9e76f
poeticalbyte <poeticalbyte@gmail.com>
2025-05-29 01:31:39 -0500

    hm

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/rayo-project-blog/',
+  base: '/rayo-project/',
   plugins: [react()],
 })
