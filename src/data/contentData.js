4493ff743c60d75142f6043bf15d6b68b3902c25
poeticalbyte <poeticalbyte@gmail.com>
2025-06-04 14:59:11 -0500

    prueba4

--- src/data/contentData.js
+++ src/data/contentData.js
@@ -2,7 +2,7 @@ const contentData = [
   {
     id: 'bio-rayo',
     title: '🧩 ¿Cómo se formó el proyecto Rayo?',
-    image: 'assets/books.jpeg',
+    image: '/books.jpeg',
     paragraphs: [
       "En 2024, Juan Ramírez y Daniel Yepez comenzaron sus estudios en Ingeniería de Sistemas en la Universidad de Nariño, sede Ipiales. Desde el inicio, compartieron una gran pasión por la computación y el aprendizaje, lo que los llevó a colaborar en distintos proyectos académicos. En 2025, durante la asignatura de Ingeniería de Software I, la docente Gloria Thomé les asignó la tarea de desarrollar un proyecto de software que resolviera un problema real.",
       "Tras analizar diversas problemáticas en su entorno, identificaron la necesidad de mejorar los procesos de aprendizaje y evaluación en la educación. Convencidos del potencial de la Inteligencia Artificial, decidieron crear un software que optimizara la enseñanza y permitiera una experiencia más personalizada para estudiantes y docentes. Así nació RAYO, una herramienta diseñada para revolucionar la educación mediante IA.",
